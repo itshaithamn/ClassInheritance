cf194536ded947f1f7b95dfb583ea4760dde27ad
Haitham Naksho <hnaksho@oakland.edu>
2024-02-01 10:21:57 -0500

    added class, methods, and main method

---
+++ Animal.java
@@ -0,0 +1,32 @@
+public class Animal {
+
+    public static void main(String[] args) {
+        
+    }
+
+    int age;
+    String gender;
+
+    static void isMammal(){
+
+    }
+
+    static void mate(){
+
+    }
+
+    class Fish{
+        int sizeInFeet;
+
+        static void canEat(){
+
+        }
+    }
+
+    class Zebra{
+        boolean is_wild = true;
+        static void run(){
+
+        }
+    }
+}
